5fa5efd7b30bf1f9da253aefe3310ab62a6ae689
Aleksandr Babkin <ababkin@ozon.ru>
2021-03-24 22:23:48 +0300

    course fixed with correct imports

--- project/app/db.py
+++ project/app/db.py
@@ -1,88 +1,30 @@
-import logging
+import logging  # new
 import os
-import ssl
 
 from fastapi import FastAPI
-from pydantic import AnyUrl, BaseModel
-from tortoise import Tortoise, run_async
+from tortoise import Tortoise, run_async  # updated
 from tortoise.contrib.fastapi import register_tortoise
 
-log = logging.getLogger("uvicorn")
-
-
-class DB(BaseModel):
-    """Just for URL parsing."""
-
-    url: AnyUrl
+log = logging.getLogger("uvicorn")  # new
 
 
 def init_db(app: FastAPI) -> None:
-    db = DB(url=os.environ.get("DATABASE_URL"))
-    ctx = ssl.create_default_context()
-    ctx.check_hostname = False
-    ctx.verify_mode = ssl.CERT_NONE
-    config = {
-        "connections": {
-            "default": {
-                "engine": "tortoise.backends.asyncpg",
-                "credentials": {
-                    "host": db.url.host,
-                    "port": db.url.port,
-                    "user": db.url.user,
-                    "password": db.url.password,
-                    "database": db.url.path.lstrip("/"),
-                    "ssl": ctx,
-                },
-            },
-        },
-        "apps": {
-            "models": {
-                "models": ["app.models.tortoise"],
-                "default_connection": "default",
-            },
-        },
-    }
-
-    register_tortoise(app, config=config)
-    # register_tortoise(
-    #     app,
-    #     db_url=os.environ.get("DATABASE_URL"),
-    #     modules={"models": ["app.models.tortoise"]},
-    #     generate_schemas=False,
-    #     add_exception_handlers=True,
-    # )
+    register_tortoise(
+        app,
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=False,
+        add_exception_handlers=True,
+    )
 
 
 # new
 async def generate_schema() -> None:
     log.info("Initializing Tortoise...")
-    db = DB(url=os.environ.get("DATABASE_URL"))
-    config2 = {
-        "connections": {
-            "default": {
-                "engine": "tortoise.backends.asyncpg",
-                "credentials": {
-                    "host": db.url.host,
-                    "port": db.url.port,
-                    "user": db.url.user,
-                    "password": db.url.password,
-                    "database": db.url.path.lstrip("/"),
-                    "ssl": ctx,
-                },
-            },
-        },
-        "apps": {
-            "models": {
-                "models": ["models.tortoise"],
-                "default_connection": "default",
-            },
-        },
-    }
 
     await Tortoise.init(
-        config=config2
-        # db_url=os.environ.get("DATABASE_URL"),
-        # modules={'models': ['models.tortoise']},
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models": ["models.tortoise"]},
     )
     log.info("Generating database schema via Tortoise...")
     await Tortoise.generate_schemas()
